378539137818ae960e8f98c1189c1a10dff5f5a7
Umar Sharief <80610051+noneofyourbusiness1415252@users.noreply.github.com>
2022-04-10 16:19:37 +0000

    fix bug in R when limit < 3

--- primes.r
+++ primes.r
@@ -8,9 +8,10 @@ total = 1
 if (limit > 7)
 	for (i in seq(3, as.integer(sqrt(limit)), 2))
 		if (sieve[i / 2]) sieve[seq(i^2 / 2, half, i)] = FALSE
-for (i in 1:half)
-	if (sieve[i]) {
-		total = total + 1
-		cat(",", 2 * i + 1)
-	}
+if (limit > 2)
+	for (i in 1:half)
+		if (sieve[i]) {
+			total = total + 1
+			cat(",", 2 * i + 1)
+		}
 cat("\nTotal:", total, "\n")
