3a4de47baecc7a9f3fd81f328973fce41e8f6a18
Umar Sharief <80610051+noneofyourbusiness1415252@users.noreply.github.com>
2022-04-23 11:24:00 +0000

    Fix bug in R

--- primes.r
+++ primes.r
@@ -9,7 +9,7 @@ if (limit > 7)
 	for (i in seq(3, as.integer(sqrt(limit)), 2))
 		if (sieve[i / 2]) sieve[seq(i^2 / 2, half, i)] = FALSE
 if (limit > 2)
-	for (i in 1:half)
+	for (i in 1:(half - 1))
 		if (sieve[i]) {
 			total = total + 1
 			cat(",", 2 * i + 1)
